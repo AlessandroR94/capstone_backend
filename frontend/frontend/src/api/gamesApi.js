206bd3c4c79ad31a23f83b051e5b4b4e90c3cd4b
AlessandroR94 <alessandrozarizzoli94@outlook.it>
2025-05-27 10:27:49 +0200

    cambiato link gameApi.js

--- frontend/frontend/src/api/gamesApi.js
+++ frontend/frontend/src/api/gamesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/api/games';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchGames = async (platform = null) => {
   if (!platform) {
