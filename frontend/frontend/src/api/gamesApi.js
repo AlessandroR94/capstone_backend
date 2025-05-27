7a9b75e93dfc55eca7e936876deaa3ffbf498359
AlessandroR94 <alessandrozarizzoli94@outlook.it>
2025-05-27 11:09:01 +0200

    update file .env

--- frontend/frontend/src/api/gamesApi.js
+++ frontend/frontend/src/api/gamesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = 'http://localhost:3001/api/games';
 
 export const fetchGames = async (platform = null) => {
   if (!platform) {
